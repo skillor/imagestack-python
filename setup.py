135145c32460d08db8e56a717d66334bef9a8fde
skillor <jack-dark1@gmx.de>
2021-06-03 18:47:19 +0200

    add setup

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='ImageStack',
+      packages=['imagestack'],
+      version='0.1',
+      description='Create Images by Stacking them',
+      author='skillor',
+      author_email='skillor@gmx.net',
+      license='MIT',
+      url='https://github.com/skillor',
+      keywords=['image', 'imagestack'],
+      classifiers=['Programming Language :: Python :: 3 :: Only',
+                   'Programming Language :: Python :: 3.7',
+                   'Programming Language :: Python :: 3.8',
+                   'Programming Language :: Python :: 3.9',
+                   'Topic :: Multimedia :: Graphics'],
+      )
